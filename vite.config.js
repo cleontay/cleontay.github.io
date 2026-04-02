20f88cfb0456d36eac23ec4ec5e56b65f3167849
cleontay <78065903+cleontay@users.noreply.github.com>
2026-04-02 15:56:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cleontay/',
+  base: '/cleontay.github.io/',
   plugins: [react()],
 })
